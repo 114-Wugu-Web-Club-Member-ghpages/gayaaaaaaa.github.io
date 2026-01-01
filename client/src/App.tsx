cf56b8c681a6c985e4a45a666e3b0be3548e8ebb
KutsunaSubaRya <index20010928@gmail.com>
2026-01-02 02:28:28 +0800

    feat: implement YAML-based note management with markdown support
    Replace hardcoded notes with YAML file management. Remove all edit/create/delete functionality as this is a static site.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import { Switch, Route } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
+import NoteDetail from "@/pages/NoteDetail";
 import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
       <Route path="/" component={Home} />
+      <Route path="/notes/:slug" component={NoteDetail} />
       <Route component={NotFound} />
     </Switch>
   );
